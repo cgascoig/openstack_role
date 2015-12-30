be7a6a19606b64490394f48ad95746b12dec64de
Chris Gascoigne <cgascoig@cisco.com>
2015-12-30 17:21:18 +1100

    add profile::nova_compute to compute role

--- manifests/os_liberty_compute.pp
+++ manifests/os_liberty_compute.pp
@@ -1,3 +1,20 @@
-class role::os_liberty_compute {
+class role::os_liberty_compute (
+  String $admin_password,
+  String $controller_host,
+) {
   include profile::base
+  
+  class { 'profile::nova_compute':
+    db_host                   => $controller_host,
+    db_password               => $admin_password,
+    
+    rabbit_host               => $controller_host,
+    rabbit_password           => $admin_password,
+    
+    glance_host               => $controller_host,
+    keystone_host             => $controller_host,
+    vncproxy_host             => $controller_host,
+    
+    neutron_keystone_password => $admin_password,
+  }
 }
